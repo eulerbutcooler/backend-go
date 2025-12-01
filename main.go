ac84d76262575e3025e7e003306b8836b43f59eb
eulerbutcooler <eulerbutcooler@gmail.com>
2025-12-01 17:57:59 +0530

    Figured out how to do path variable extraction and query params

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -35,19 +36,54 @@ func logMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+// Understanding query parameters
+// now if we get a request on url - localhost:8080/?name=amaan then
+// it will print home sweet home amaan
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "home sweet home")
+	query := r.URL.Query()
+	name := query.Get("name")
+	if name == "" {
+		name = "Guest"
+	}
+	fmt.Fprintf(w, "home sweet home %s", name)
 }
 
+// Extracting path variables
+// Example - https://localhost:8080/about/123
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "about last night")
+	pathSegments := strings.Split(r.URL.Path, "/")
+	if len(pathSegments) >= 3 && pathSegments[1] == "about" {
+		userID := pathSegments[2]
+		fmt.Fprintf(w, "User ID: %s", userID)
+	} else {
+		http.NotFound(w, r)
+	}
+	// fmt.Fprintln(w, "about last night")
+}
+
+// Combining both query params and path variables
+// http://localhost:8080/username/123?includedetails=true
+func usernameHandler(w http.ResponseWriter, r *http.Request) {
+	pathSeg := strings.Split(r.URL.Path, "/")
+	includeDets := r.URL.Query().Get("includedetails")
+	if len(pathSeg) >= 3 && pathSeg[1] == "username" {
+		userId := pathSeg[2]
+		fmt.Fprintf(w, "User id: %s\n", userId)
+		if includeDets == "true" {
+			w.Write([]byte("Details are included\n"))
+		}
+	} else {
+		http.NotFound(w, r)
+	}
+
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", logMiddleware(headerMiddleware(http.HandlerFunc(homeHandler))))
-	mux.Handle("/about", logMiddleware(headerMiddleware(http.HandlerFunc(aboutHandler))))
-
+	mux.Handle("/about/", logMiddleware(headerMiddleware(http.HandlerFunc(aboutHandler))))
+	mux.Handle("/username/", logMiddleware(headerMiddleware(http.HandlerFunc(usernameHandler))))
 	// localhost:8080/api -> called -> handler -> function
 	mux.HandleFunc("/api", apiHandler)
 	log.Printf("Starting server at port %d", port)
