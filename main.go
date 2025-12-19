634e21a75e6e7bb0a9287f07bae8c71dd95b7e26
amaan <eulerbutcooler@gmail.com>
2025-12-19 17:14:35 +0530

    Added a validate user method

--- main.go
+++ main.go
@@ -24,6 +24,38 @@ var (
 	mutex = &sync.Mutex{}
 )
 
+func (user User) Validate() error {
+	if user.Name==""{
+		return fmt.Errorf("missing field: name")
+	}
+	if user.Email==""{
+		return fmt.Errorf("missing field: email")
+	}
+	return nil
+}
+
+func validateHandler(w http.ResponseWriter, r *http.Request){
+	var user User
+	decoder :=json.NewDecoder(r.Body)
+	err:=decoder.Decode(&user)
+	if err!=nil{
+		http.Error(w, "Invalid Request Payload", http.StatusBadRequest)
+		return
+	}
+	err=user.Validate()
+	if err!=nil{
+		response:=map[string]string{"error":err.Error()}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		err:=json.NewEncoder(w).Encode(response); err!=nil {
+			http.Error(w,"Failed to encode json response", http.StatusInternalServerError)
+		}
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("User data is valid"))
+}
+
 func usersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	switch r.Method {
